5391e19c147c62a87d82aaa73833a7c565064433
Andrey Kobyshev <yokotoka@andreys-imac.local>
2016-02-27 14:49:10 +0300

    history and record

--- Water Landing/ViewController.swift
+++ Water Landing/ViewController.swift
@@ -8,14 +8,53 @@
 
 import UIKit
 
+class Record {
+    var datetime: time_t = 0
+    var drunked: Int = 0
+    
+    init (datetime:time_t, drunked:Int) {
+        self.datetime = datetime
+        self.drunked = drunked
+    }
+}
+
+class History {
+    var records : [Record] = []
+    
+    func add_record(record:Record){
+        self.records.append(record)
+    }
+    
+    func sum_records() -> Int {
+        var sum: Int = 0
+        for record in self.records {
+            sum += record.drunked
+        }
+        return sum
+    }
+    
+}
+
+
 class ViewController: UIViewController {
     
+    
     func addWater(doze: Int) {
-        balance += doze
-        label1.text = String(balance)
+        //balance += doze
+        
+        
+        
+        //label1.text = String(balance)
+        
+        
+        var record = Record(datetime: 0, drunked: doze)
+        self.history.add_record(record)
+        label1.text = String(self.history.sum_records())
+        
     }
     
     var balance = 0
+    var history:History = History()
 
     @IBOutlet weak var label1: UILabel!
     
@@ -37,6 +76,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
