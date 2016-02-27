0a1ccf01fc4b8fc6332f4106318d41ef1a1c4cf3
Артем <artem@artems-imac.local>
2016-02-27 12:19:53 +0300

    Initial Commit

---
+++ Water LandingTests/Water_LandingTests.swift
@@ -0,0 +1,36 @@
+//
+//  Water_LandingTests.swift
+//  Water LandingTests
+//
+//  Created by Артем on 27/02/16.
+//  Copyright © 2016 Артем. All rights reserved.
+//
+
+import XCTest
+@testable import Water_Landing
+
+class Water_LandingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
